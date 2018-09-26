7a8722d50ffc5928ca0d21091040c6758244dd6c
Frederic Branczyk <fbranczyk@gmail.com>
2018-09-26 22:42:43 +0200

    Fix license headers

--- pkg/authn/config.go
+++ pkg/authn/config.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 Frederic Branczyk All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package authn
 
 // AuthnHeaderConfig contains authentication header settings which enable more information about the user identity to be sent to the upstream
@@ -14,7 +30,7 @@ type AuthnHeaderConfig struct {
 	GroupSeparator string
 }
 
-// AuthnConfig holds all configurations related to authentication options 
+// AuthnConfig holds all configurations related to authentication options
 type AuthnConfig struct {
 	X509   *X509Config
 	Header *AuthnHeaderConfig
