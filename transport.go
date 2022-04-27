e8ff47a0faf3a19fe467bfc56919b19113758996
Haoyu Sun <hasun@redhat.com>
2022-04-27 12:50:37 +0200

    initTransport() uses http.DefaultTransport for initialization.

--- transport.go
+++ transport.go
@@ -22,9 +22,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"net"
 	"net/http"
-	"time"
 )
 
 func initTransport(upstreamCAFile string) (http.RoundTripper, error) {
@@ -42,20 +40,9 @@ func initTransport(upstreamCAFile string) (http.RoundTripper, error) {
 		return nil, errors.New("error parsing upstream CA certificate")
 	}
 
-	// http.Transport sourced from go 1.10.7
-	transport := &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-		TLSClientConfig:       &tls.Config{RootCAs: roots},
-	}
+	transport := (http.DefaultTransport.(*http.Transport)).Clone()
+	transport.TLSClientConfig = &tls.Config{RootCAs: roots}
+	transport.ForceAttemptHTTP2 = false
 
 	return transport, nil
 }
