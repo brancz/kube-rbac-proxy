209057726bc254eaf90c366745c8335b1ebc4e9d
Luca Comellini <luca.com@gmail.com>
2025-03-31 14:05:47 -0700

    Update golangci-lint to v2

--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 licRes=$(
-for file in $(find . -type f -iname '*.go' ! -path '*/vendor/*'); do
-	head -n3 "${file}" | grep -Eq "(Copyright|generated|GENERATED)" || echo -e "  ${file}"
-done;)
+	for file in $(find . -type f -iname '*.go' ! -path '*/vendor/*'); do
+		head -n5 "${file}" | grep -Eq "(Copyright|generated|GENERATED)" || printf "  %s\n" "${file}"
+	done
+)
 if [ -n "${licRes}" ]; then
-	echo -e "license header checking failed:\n${licRes}"
+	printf "license header checking failed:\n%s\n" "${licRes}"
 	exit 255
 fi
