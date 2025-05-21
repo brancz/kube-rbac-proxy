c1dd005d194b6748933fd1312270d46d98fd1ebe
Krzysztof Ostrowski <kostrows@redhat.com>
2025-05-21 16:43:53 +0200

    test/e2e: streamline changes, uncomment tests

--- test/e2e/templated_query_rewrite_static.go
+++ test/e2e/templated_query_rewrite_static.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Frederic Branczyk All rights reserved.
+Copyright 2025 The kube-rbac-proxy maintainers. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
